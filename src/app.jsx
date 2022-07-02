a0b3d01c37fcabebe533a383c0d57f057a9d2618
EavOutDom <eavoutdom400@gmail.com>
2022-07-02 22:57:34 +0700

    commit 3

--- src/app.jsx
+++ src/app.jsx
@@ -4,8 +4,8 @@ import questions from "./data";
 function App() {
     return (
         <div className="mt-20 flex justify-center">
-            <div className="bg-white min-h-80 w-[42rem] p-3 flex rounded-xl shadow-xl">
-                <h2 className="w-60 font-bold p-2 text-2xl">
+            <div className="bg-white min-h-80 md:w-[42rem] duration-500 w-80 p-2 md:flex rounded-xl shadow-xl">
+                <h2 className="w-60 font-bold p-2 text-xl md:text-2xl duration-500">
                     Questions And Answers About Login
                 </h2>
                 <div className="p-2">
--- src/question.jsx
+++ src/question.jsx
@@ -3,15 +3,16 @@ import { BsFillPatchMinusFill, BsFillPatchPlusFill } from "react-icons/bs";
 const Question = ({ info, title }) => {
     const [show, setShow] = useState(false);
     return (
-        <div className="py-4 p-6 ring-1 ring-gray-400 mb-3 rounded-md shadow-md max-w-md">
+        <div className="p-4 ring-1 ring-gray-400 mb-3 rounded-md shadow-md max-w-md">
             <div>
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-start">
                     <div>
                         <h2 className="font-bold">{title}</h2>
+                        <div>{show && <p>{info}</p>}</div>
                     </div>
-                    <div className="flex items-cent ">
+                    <div className="flex items-center">
                         <button
-                        className="hover:text-slate-400"
+                            className="md:hover:text-slate-400 mx-2 my-2 text-3xl md:text-base duration-500"
                             onClick={() => {
                                 if (show) {
                                     setShow(false);
@@ -20,11 +21,14 @@ const Question = ({ info, title }) => {
                                 }
                             }}
                         >
-                           {show ? <BsFillPatchMinusFill /> : <BsFillPatchPlusFill/>} 
+                            {show ? (
+                                <BsFillPatchMinusFill />
+                            ) : (
+                                <BsFillPatchPlusFill />
+                            )}
                         </button>
                     </div>
                 </div>
-                <div>{show && <p>{info}</p>}</div>
             </div>
         </div>
     );
